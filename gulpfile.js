38695fc56f37fe4dd53bf11af5c9bd503f4706a1
Rowan Oulton <rowan.oulton@gmail.com>
2014-06-05 17:02:18 +1200

    Add imagery + imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-var gulp    = require('gulp'),
-    compass = require('gulp-compass');
+var gulp     = require('gulp'),
+    compass  = require('gulp-compass'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('compass', function () {
-    gulp.src('./src/scss/*.scss')
+    return gulp.src('./src/scss/*.scss')
         .pipe(compass({
             config_file: './compass.rb',
             css: 'assets/css',
@@ -11,8 +12,15 @@ gulp.task('compass', function () {
         .pipe(gulp.dest('./assets/css'));
 });
 
+gulp.task('images', function () {
+    return gulp.src('./src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./assets/img'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('./src/scss/**/*.scss', ['compass']);
+    gulp.watch('./src/img/*', ['images']);
 });
 
 gulp.task('default', ['watch']);
